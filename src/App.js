94db521ae57f11ce9cccb671ebe00788b4d46dd7
ryotogashi <ryo.togashi.ca@gmail.com>
2019-08-21 16:50:56 -0700

    broke down homepage components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HomePage from "./hompage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
